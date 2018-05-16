b0bfb211f4e61fcb647ac46b6ce04296bdcbad87
Sam Jenkins <sam.jenko@hotmail.com>
2018-05-16 14:49:19 +0100

    exercise completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: "Strada", address: "Strada Street", phone_number: "123456789", category: "italian")
+Restaurant.create(name: "Pier Victoire", address: "Pv Street", phone_number: "23456789", category: "french")
+Restaurant.create(name: "Yumyum", address: "Yum Street", phone_number: "3456789", category: "chinese")
+Restaurant.create(name: "Belg", address: "Belg Street", phone_number: "456789", category: "belgian")
+Restaurant.create(name: "Yo Sushi", address: "Yo Street", phone_number: "56789", category: "japanese")
